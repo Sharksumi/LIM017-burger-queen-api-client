e97648c1c22b01b785e624bd0436f966b9e68455
unknown <llopez@caleidos.pe>
2023-04-18 17:31:43 -0500

    prueba appspec.yml

--- before-install.sh
+++ before-install.sh
@@ -3,6 +3,6 @@
 sysdate=$(date +"%Y%m%d%H%M")
 dir_to_zip="/var/www/html/vue_sinergia"
 zip -r "${dir_to_zip}_${sysdate}.zip" $dir_to_zip
-mkdir /home/flesan/backups
+mkdir -p /home/flesan/backups
 cd /home/flesan/backups
 mv "${dir_to_zip}_${sysdate}.zip" /home/flesan/backups/
