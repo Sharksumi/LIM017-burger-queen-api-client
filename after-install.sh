537d40fa837c1999f7e793dc190594cfe523d7f4
unknown <llopez@caleidos.pe>
2023-04-18 12:41:10 -0500

    prueba appspec.yml

--- after-install.sh
+++ after-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/html/vue_sinergia
-cp -R /var/www/html/vue_sinergia_backup/. .
+# cp -R /var/www/html/vue_sinergia_backup/. .
+rsync -ah --ignore-existing --no-inc-recursive /var/www/html/vue_sinergia_backup/ .
 # php artisan config:clear
 # php artisan cache:clear
 # php artisan migrate
