ae53018d9792338c293aa44d45045ca05ece5b37
unknown <llopez@caleidos.pe>
2023-04-18 17:19:45 -0500

    prueba appspec.yml

--- before-install.sh
+++ before-install.sh
@@ -2,7 +2,7 @@
 
 sysdate=$(date +"%Y%m%d%H%M")
 dir_to_zip="/var/www/html/vue_sinergia"
-sudo zip -r "${dir_to_zip}_${sysdate}.zip" $dir_to_zip
+zip -r "${dir_to_zip}_${sysdate}.zip" $dir_to_zip
 mkdir /var/www/html/versions
 cd /var/www/html/versions
 mv "${dir_to_zip}_${sysdate}.zip" /var/www/html/versions/
